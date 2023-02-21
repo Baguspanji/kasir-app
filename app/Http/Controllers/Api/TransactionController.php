95fe7ecf123caac3e873d0d9a38ee61114cb70f9
baguspanji <pituns060@gmail.com>
2023-02-21 12:33:25 +0700

    update download data

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -12,6 +12,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Facades\Excel;
@@ -342,6 +343,10 @@ class TransactionController extends Controller
             return response()->json($response, 500);
         }
 
-        return response()->download(storage_path('app/public/export/' . $name));
+        $headers = array(
+            'Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        );
+
+        return Response::download(storage_path('app/public/export/' . $name), $name, $headers);
     }
 }
